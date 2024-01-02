4e776bfbdd8d26d72e726744534f6e4ff245df80
Geeth-Rath <geeth4862@gmail.com>
2024-01-02 21:07:46 +0530

    commited

--- src/main/java/org/example/Anagram.java
+++ src/main/java/org/example/Anagram.java
@@ -1,4 +1,24 @@
 package org.example;
 
 public class Anagram {
+    public void isAnagram(){
+        String str1 = "Race";
+        String str2 = "Care";
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        System.out.println(str1);
+        System.out.println(str2);
+
+        for (int i = 0; i < str1.length()-1; i++) {
+            for (int j = i; j < str1.length()-1; j++) {
+                String temp;
+//                temp =  str1[i];
+
+            }
+        }
+
+
+
+
+    }
 }
